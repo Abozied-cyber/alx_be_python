8acd02dbc63da52b947cab9e37e4b7b440bbff88
Abozied-cyber <mohamedabozed668@gmail.com>
2025-03-06 10:32:15 +0200

    modify simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,15 +1,9 @@
 #calculates the simple interest earned on an investment over 3 years
 
 
-principal = p = 1000
-rate = R = 0.05
-time = T = 3
-interest = p*R*T
-
-
-
-
-
-
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal*rate*time
 
 print(f"The simple interest is: {interest}")
